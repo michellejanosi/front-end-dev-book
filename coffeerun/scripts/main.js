704c0bd04510416184b95b852b6c5da3f50e16d8
michellejanosi <michelle.janosi@gmail.com>
2017-03-12 11:22:11 -0400

    Added order form validations

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -6,6 +6,7 @@
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  var Validation = App.Validation;
   var CheckList = App.CheckList;
   var myTruck = new Truck('ncc-1701', new DataStore());
   window.myTruck = myTruck;
@@ -13,9 +14,10 @@
   checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
   var formHandler = new FormHandler(FORM_SELECTOR);
 
-  formHandler.addSubmitHandler(function (data) {
+  formHandler.addSubmitHandler(function(data) {
     myTruck.createOrder(data);
     checkList.addRow(data);
   });
-  console.log(formHandler);
+
+  formHandler.addInputHandler(Validation.isCompanyEmail);
 })(window);
